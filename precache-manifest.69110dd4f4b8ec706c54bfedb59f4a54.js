c517defebd4a4885e0418db8f082ee357261c73c
Ashfiii <alam19231@gmail.com>
2020-08-26 00:37:28 +0530

    Updates

---
+++ precache-manifest.69110dd4f4b8ec706c54bfedb59f4a54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770623322609e013625e89602b91a3c9",
+    "url": "/Ashfiii/breaking-bad.git/index.html"
+  },
+  {
+    "revision": "10a5fe0b8eef38ce9802",
+    "url": "/Ashfiii/breaking-bad.git/static/css/main.ed61c270.chunk.css"
+  },
+  {
+    "revision": "0808bc06365460b710c8",
+    "url": "/Ashfiii/breaking-bad.git/static/js/2.33e7f648.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Ashfiii/breaking-bad.git/static/js/2.33e7f648.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10a5fe0b8eef38ce9802",
+    "url": "/Ashfiii/breaking-bad.git/static/js/main.77e9987d.chunk.js"
+  },
+  {
+    "revision": "9f6071351ddda9925e84",
+    "url": "/Ashfiii/breaking-bad.git/static/js/runtime-main.60d1cd77.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/Ashfiii/breaking-bad.git/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/Ashfiii/breaking-bad.git/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/Ashfiii/breaking-bad.git/static/media/spinner.235b9b3e.gif"
+  }
+]);
